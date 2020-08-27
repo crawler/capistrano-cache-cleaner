76ebbae4e4d8e5fdc4a60ddff27e8f5e678b00eb
Anton Topchii <player1@infinitevoid.net>
2020-08-27 15:13:59 +0300

    files rename

--- lib/capistrano/cache.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require 'capistrano'
-import File.join(__dir__, 'tasks/cache.rake')
---
+++ lib/capistrano/cache_cleaner.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+require 'capistrano'
+import File.join(__dir__, 'tasks/cache_cleaner.rake')
